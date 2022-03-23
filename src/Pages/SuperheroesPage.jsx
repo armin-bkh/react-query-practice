479a3976d0276d7d14cbcabdc046fe890b11c6e9
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-03-24 02:22:10 +0430

    handle get request for superheroes page

--- src/Pages/SuperheroesPage.jsx
+++ src/Pages/SuperheroesPage.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../Layout/Layout";
+import http from "../Services/httpServices";
 
 function SuperheroesPage() {
+  const [superheroes, setSuperheroes] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    http
+      .get("/superheroes")
+      .then(({ data }) => {
+        setLoading(false);
+        setSuperheroes(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <Layout>
       <div>SuperheroesPage</div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : !error && superheroes ? (
+        superheroes.map((superhero) => <p>{superhero.name}</p>)
+      ) : (
+        <h1>{error}</h1>
+      )}
     </Layout>
   );
 }
